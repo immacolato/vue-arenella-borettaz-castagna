bff559bd6d0cd4cbccdab737c9b72d1d54f1fb3c
Natalia Borettaz <nat.borettaz@gmail.com>
2023-09-06 20:53:36 +0200

    feat: add sign up component

---
+++ src/components/SignUp.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="register">
+    <img class="logo" src="../assets/logoTennis.png" />
+    <h1>Sign Up</h1>
+    <input type="text" placeholder="enter name" />
+    <input type="text" placeholder="enter email" />
+    <input type="password" placeholder="enter password" />
+    <button>Sign Up</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp'
+}
+</script>
+
+<style>
+.logo {
+  width: 100px;
+}
+.register {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.register button {
+  width: 320px;
+  height: 40px;
+  border: 1px solid green;
+  background: lightgreen;
+  cursor: pointer;
+}
+</style>
