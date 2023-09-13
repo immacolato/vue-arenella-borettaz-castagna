5d5509dff3ccb2cb87a120ba77cf6f446fa1b1bd
Giovanni Castagna <giovanni.castagna@edu.unito.it>
2023-09-13 22:46:06 +0200

    Feat: add Navbar

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="navbar navbar-dark bg-success">
+        <a class="navbar-brand" href="#">Home</a>
+        <a class="navbar-brand" href="#">Aggiungi Campo</a>
+        <a class="navbar-brand" href="#">Modifica Campo</a>
+        <a class="navbar-brand" href="#">Esci</a>
+    </nav>
+</template> 
+    <script>
+    export default {
+        name:"SiteHeader"
+    }
+    </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,16 @@
 <template>
+    <SiteHeader />
     <h1 class="text-center mt-5">Ciao Utente, benvenuta/o in Tennis Hub</h1>
 </template>
     
 <script>
+import SiteHeader from "./Header.vue"
 export default {
     name: "HomePage",
-    mounted() {
+    components: {
+        SiteHeader
+    },
+mounted() {
         let user = localStorage.getItem('user-info');
         if (!user)
             this.$router.push({ name: 'SignUp' })
