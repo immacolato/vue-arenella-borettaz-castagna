a3df2f25b4d85bc41b95dc8a07970b239fc3c447
Natalia Borettaz <nat.borettaz@gmail.com>
2023-09-17 20:34:22 +0200

    feat: add the table

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,48 @@
 <template>
-    <SiteHeader />
-    <h1 class="text-center mt-5">Ciao {{ name }}, benvenuta/o in Tennis Hub</h1>
+  <SiteHeader />
+  <h1 class="text-center mt-5">Ciao {{ name }}, benvenuta/o in Tennis Hub</h1>
+  <table class="mt-5 table table-bordered">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Contatti</th>
+        <th scope="col">Indirizzo</th>
+      </tr>
+    </thead>
+    <tbody class="table-group-divider"></tbody>
+    <tr v-for="item in tennisField" :key="item.id">
+      <th scope="row">{{ item.id }}</th>
+      <td>{{ item.name }}</td>
+      <td>{{ item.contact }}</td>
+      <td>{{ item.address }}</td>
+    </tr>
+  </table>
 </template>
-    
+
 <script>
-import SiteHeader from "./Header.vue"
+import SiteHeader from './Header.vue'
+import axios from 'axios'
 export default {
-    name: "HomePage",
-    data() {
-        return {
-            name: ''
-        }
-    },
-    components: {
-        SiteHeader
-    },
-    mounted() {
-        let user = localStorage.getItem("user-info");
-        this.name = JSON.parse(user)[0].name;
-        if (!user)
-            this.$router.push({ name: 'SignUp' })
+  name: 'HomePage',
+  data() {
+    return {
+      name: '',
+      tennisField: []
     }
+  },
+  components: {
+    SiteHeader
+  },
+  async mounted() {
+    let user = localStorage.getItem('user-info')
+    this.name = JSON.parse(user)[0].name
+    if (!user) {
+      this.$router.push({ name: 'SignUp' })
+    }
+    let result = await axios.get('http://localhost:3000/tennisField')
+    console.warn(result)
+    this.tennisField = result.data
+  }
 }
 </script>
