95fbf9f4af9453cc79c2eba494614ec511eb4438
Giovanni Castagna <giovanni.castagna@edu.unito.it>
2023-09-18 21:14:52 +0200

    feat: bootstrap form aggiungi campo

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,14 +1,17 @@
 <template>
     <SiteHeader />
-    <h1 class="text-center mt-5">Ciao Utente, benvenuta/o nella pagina di aggiunta campi</h1>
-    <form>
-      <input type="text" name="name" placeholder="Inserisci il nome" v-model="campo.name"/>
-      <input type="text" name="address" placeholder="Inserisci l'indirizzo" v-model="campo.address"/>
-      <input type="text" name="contact" placeholder="Inserisci il contatto" v-model="campo.contact"/>  
-      <button type="button" v-on:click="addCampo">Aggiungi un nuovo campo</button>    
+    <h1 class="text-center mt-5">Ciao {{ name }}, benvenuta/o nella pagina di aggiunta campi</h1>
+<div class="mt-5 container" style="max-width: 720px; margin: 0 auto;">
+    <form class="row g-3">
+      <input class="form-control" type="text" name="name" placeholder="Inserisci il nome del campo" v-model="campo.name"/>
+      <input class="form-control" type="text" name="contact" placeholder="Inserisci contatto" v-model="campo.contact"/> 
+      <input class="form-control" type="text" name="address" placeholder="Inserisci l'indirizzo del campo" v-model="campo.address"/>
+      <button class="mt-3 btn btn-success shadow-sm" type="button" v-on:click="addCampo" style="max-width: 420px; margin: 0 auto;">
+        Aggiungi nuovo campo</button>    
     </form>
+</div>
 </template>
-    
+
 <script>
 import SiteHeader from "./Header.vue"
 export default {
