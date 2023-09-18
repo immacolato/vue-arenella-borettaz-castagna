2b9151e9770c0d322fadf8afdce6b34d048bb67a
Luca Arenella <luca.arenella@edu.unito.it>
2023-09-18 21:54:37 +0200

    feat: added name to add page

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,15 +1,19 @@
 <template>
     <SiteHeader />
     <h1 class="text-center mt-5">Ciao {{ name }}, benvenuta/o nella pagina di aggiunta campi</h1>
-<div class="mt-5 container" style="max-width: 720px; margin: 0 auto;">
-    <form class="row g-3">
-      <input class="form-control" type="text" name="name" placeholder="Inserisci il nome del campo" v-model="campo.name"/>
-      <input class="form-control" type="text" name="contact" placeholder="Inserisci contatto" v-model="campo.contact"/> 
-      <input class="form-control" type="text" name="address" placeholder="Inserisci l'indirizzo del campo" v-model="campo.address"/>
-      <button class="mt-3 btn btn-success shadow-sm" type="button" v-on:click="addCampo" style="max-width: 420px; margin: 0 auto;">
-        Aggiungi nuovo campo</button>    
-    </form>
-</div>
+    <div class="mt-5 container" style="max-width: 720px; margin: 0 auto;">
+        <form class="row g-3">
+            <input class="form-control" type="text" name="name" placeholder="Inserisci il nome del campo"
+                v-model="campo.name" />
+            <input class="form-control" type="text" name="contact" placeholder="Inserisci contatto"
+                v-model="campo.contact" />
+            <input class="form-control" type="text" name="address" placeholder="Inserisci l'indirizzo del campo"
+                v-model="campo.address" />
+            <button class="mt-3 btn btn-success shadow-sm" type="button" v-on:click="addCampo"
+                style="max-width: 420px; margin: 0 auto;">
+                Aggiungi nuovo campo</button>
+        </form>
+    </div>
 </template>
 
 <script>
@@ -19,27 +23,28 @@ export default {
     components: {
         SiteHeader
     },
-    data()
-    {
+    data() {
         return {
             campo: {
-                name:'',
-                address:'',
-                contact:''
-            }
+                name: '',
+                address: '',
+                contact: '',
+            },
+            name: ''
         }
     },
-methods:{
-        addCampo()
-        {
+    methods: {
+        addCampo() {
             console.warn(this.campo)
         }
     },
 
-    mounted() {
-        let user = localStorage.getItem('user-info');
-        if (!user)
+    async mounted() {
+        let user = localStorage.getItem('user-info')
+        this.name = JSON.parse(user)[0].name //[0] aggiunto perchè nel db gli utenti sono dentro un array e non un oggetto
+        if (!user) {
             this.$router.push({ name: 'SignUp' })
+        }
     }
 }
 </script>
