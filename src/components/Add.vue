ce283a8dc22ce4867376921a1d27fc814c7257ca
Giovanni Castagna <giovanni.castagna@edu.unito.it>
2023-09-18 19:46:16 +0200

    feat: form per aggiungere campi da tennis

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,6 +1,12 @@
 <template>
     <SiteHeader />
     <h1 class="text-center mt-5">Ciao Utente, benvenuta/o nella pagina di aggiunta campi</h1>
+    <form>
+      <input type="text" name="name" placeholder="Inserisci il nome" v-model="campo.name"/>
+      <input type="text" name="address" placeholder="Inserisci l'indirizzo" v-model="campo.address"/>
+      <input type="text" name="contact" placeholder="Inserisci il contatto" v-model="campo.contact"/>  
+      <button type="button" v-on:click="addCampo">Aggiungi un nuovo campo</button>    
+    </form>
 </template>
     
 <script>
@@ -10,6 +16,23 @@ export default {
     components: {
         SiteHeader
     },
+    data()
+    {
+        return {
+            campo: {
+                name:'',
+                address:'',
+                contact:''
+            }
+        }
+    },
+methods:{
+        addCampo()
+        {
+            console.warn(this.campo)
+        }
+    },
+
     mounted() {
         let user = localStorage.getItem('user-info');
         if (!user)
