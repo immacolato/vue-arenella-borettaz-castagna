6fb8ec34b817992b35ca4216f65d89d30c8a8a36
Giovanni Castagna <giovanni.castagna@edu.unito.it>
2023-09-20 20:34:48 +0200

    feat: implementato la funzione aggiorna

--- src/components/Update.vue
+++ src/components/Update.vue
@@ -1,14 +1,12 @@
 <template>
     <SiteHeader />
-    <h1 class="text-center mt-5">Ciao {{ name }}, benvenuta/o nella pagina di modifica dei campi</h1>
+    <h1 class="text-center mt-5">Ciao {{ name }}, modifica il campo</h1>
     <div class="mt-5 container" style="max-width: 678px; margin: 0 auto">
     <form class="row g-3">
-      <input class="form-control" type="text" name="name" placeholder="Inserisci il nome del campo"
-        v-model="campo.name" />
+      <input class="form-control" type="text" name="name" placeholder="Inserisci il nome del campo" v-model="campo.name" />
       <input class="form-control" type="text" name="contact" placeholder="Inserisci contatto" v-model="campo.contact" />
-      <input class="form-control" type="text" name="address" placeholder="Inserisci l'indirizzo del campo"
-        v-model="campo.address" />
-      <button class="mt-3 btn btn-success shadow-sm" type="button" v-on:click="addCampo"
+      <input class="form-control" type="text" name="address" placeholder="Inserisci l'indirizzo del campo" v-model="campo.address" />
+      <button class="mt-3 btn btn-success shadow-sm" type="button" v-on:click="aggiornaCampo"
         style="max-width: 420px; margin: 0 auto">
         Modifica campo
       </button>
@@ -16,8 +14,9 @@
   </div>
 </template>
     
-<script>
+<script> 
 import SiteHeader from "./Header.vue"
+import axios from 'axios'
 export default {
     name: "UpdatePage",
     components: {
@@ -33,10 +32,33 @@ export default {
       name: ''
     }
   },
-    mounted() {
+
+  methods:{
+    async aggiornaCampo()
+    { 
+      console.warn(this.campo)
+      const result = await axios.put('http://localhost:3000/tennisField/'+this.$route.params.id, {
+        name: this.campo.name,
+        address: this.campo.address,
+        contact: this.campo.contact
+      })
+      if (result.status == 200) {
+        this.$router.push({ name: 'Home' })
+      }
+    }
+  },
+
+    async mounted() 
+    {
         let user = localStorage.getItem('user-info');
         if (!user)
+        {
             this.$router.push({ name: 'SignUp' })
     }
+    const result = await axios.get('http://localhost:3000/tennisField/'+this.$route.params.id);
+    // console.warn(this.$route.params.id)
+    console.warn(result.data)
+    this.campo=result.data
+}
 }
 </script>
