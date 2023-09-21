648835a0f06165d1881e488c728f0292ee280e60
Luca Arenella <luca.arenella@edu.unito.it>
2023-09-21 14:32:48 +0200

    Feat: changed alert on update

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,24 +1,37 @@
 <template>
-  <SiteHeader />
-  <h1 class="text-center mt-5">Ciao {{ name }}, benvenuta/o nella pagina di aggiunta campi</h1>
-  <div class="mt-5 container" style="max-width: 678px; margin: 0 auto">
-    <form class="row g-3">
-      <input class="form-control" type="text" name="name" placeholder="Inserisci il nome del campo"
-        v-model="campo.name" />
-      <input class="form-control" type="text" name="contact" placeholder="Inserisci contatto" v-model="campo.contact" />
-      <input class="form-control" type="text" name="address" placeholder="Inserisci l'indirizzo del campo"
-        v-model="campo.address" />
-      <button class="mt-3 btn btn-success shadow-sm" type="button" v-on:click="addCampo"
-        style="max-width: 420px; margin: 0 auto">
-        Aggiungi nuovo campo
+  <div>
+    <SiteHeader />
+    <h1 class="text-center mt-5">Ciao {{ name }}, benvenuta/o nella pagina di aggiunta campi</h1>
+
+    <!-- Alert personalizzato Bootstrap -->
+    <div v-if="showAlert" class="text-center alert alert-success alert-dismissible" role="alert">
+      <strong>Messaggio:</strong> Campo aggiunto con successo!
+      <button type="button" class="close" @click="showAlert = false" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
       </button>
-    </form>
+    </div>
+
+
+    <div class="mt-5 container" style="max-width: 678px; margin: 0 auto">
+      <form class="row g-3">
+        <input class="form-control" type="text" name="name" placeholder="Inserisci il nome del campo"
+          v-model="campo.name" />
+        <input class="form-control" type="text" name="contact" placeholder="Inserisci contatto" v-model="campo.contact" />
+        <input class="form-control" type="text" name="address" placeholder="Inserisci l'indirizzo del campo"
+          v-model="campo.address" />
+        <button class="mt-3 btn btn-success shadow-sm" type="button" @click="addCampo"
+          style="max-width: 420px; margin: 0 auto">
+          Aggiungi nuovo campo
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 import SiteHeader from './Header.vue'
 import axios from 'axios'
+
 export default {
   name: 'AddPage',
   components: {
@@ -31,41 +44,49 @@ export default {
         address: '',
         contact: ''
       },
-      name: ''
-    }
+      name: '',
+      showAlert: false
+    };
   },
   methods: {
     async addCampo() {
-      console.warn(this.campo)
-      const result = await axios.post('http://localhost:3000/tennisField', {
-        name: this.campo.name,
-        address: this.campo.address,
-        contact: this.campo.contact
-      })
-      if (result.status == 201) {
-        // Mostra un alert
-        alert('Campo aggiunto con successo!');
+      console.warn(this.campo);
+      try {
+        const result = await axios.post('http://localhost:3000/tennisField', {
+          name: this.campo.name,
+          address: this.campo.address,
+          contact: this.campo.contact
+        });
+        if (result.status == 201) {
+          // Mostra l'alert
+          this.showAlert = true;
+          // Nascondi l'alert automaticamente dopo 3 secondi
+          setTimeout(this.hideAlert, 3000);
 
-        // Svuota il form
-        this.campo.name = '';
-        this.campo.address = '';
-        this.campo.contact = '';
+          // Svuota il form
+          this.campo.name = '';
+          this.campo.address = '';
+          this.campo.contact = '';
 
-        // Reindirizza l'utente alla pagina 'Home'
-        this.$router.push({ name: 'Home' });
-      } else {
+        } else {
+          alert('Si è verificato un errore durante l\'aggiunta del campo.');
+        }
+        console.warn('result', result);
+      } catch (error) {
+        console.error('Errore durante la richiesta:', error);
         alert('Si è verificato un errore durante l\'aggiunta del campo.');
       }
-      console.warn('result', result)
     }
   },
-
   async mounted() {
     let user = localStorage.getItem('user-info')
     this.name = JSON.parse(user)[0].name //[0] aggiunto perchè nel db gli utenti sono dentro un array e non un oggetto
     if (!user) {
       this.$router.push({ name: 'SignUp' })
     }
+  },
+  hideAlert() {
+    this.showAlert = false;
   }
 }
 </script>
