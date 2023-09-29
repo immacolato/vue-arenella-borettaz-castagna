8493636f60252521a38c4a9d61ee6e80ed415749
Luca Arenella <luca.arenella@edu.unito.it>
2023-09-29 17:06:46 +0200

    fix: minor changes in main.js

--- src/main.js
+++ src/main.js
@@ -2,20 +2,25 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers'
 import 'bootstrap/dist/css/bootstrap.css'
-/* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
-/* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-/* import specific icons */
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
-/* add icons to the library */
-library.add(faUserSecret, faTrash, faPenToSquare)
-createApp(App).use(router).mount('#app')
 
-router.push({ name: 'LoginPage' })
-const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon)
+// Aggiungi le icone alla libreria
+library.add(faUserSecret, faTrash, faPenToSquare)
+
+const app = createApp(App)
+
+// Usa il router nell'app
 app.use(router)
 
+// Registra il componente FontAwesomeIcon in modo globale
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+// Monta l'app nell'elemento con ID 'app'
 app.mount('#app')
+
+// Imposta la rotta iniziale (LoginPage) dopo aver montato l'app
+router.push({ name: 'LoginPage' })
